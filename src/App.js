f9054266d42865fee41fe52ecf33fc1c7689e8f6
Chetana1911 <142580638+Chetana1911@users.noreply.github.com>
2024-09-08 16:12:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
